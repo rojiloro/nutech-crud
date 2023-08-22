329bbd46ab1df88ae5514147977f64e88ed49a21
rojiloro <kelengoji79@gmail.com>
2023-08-22 20:53:07 +0700

    cek

--- server/models/product.go
+++ server/models/product.go
@@ -3,7 +3,7 @@ package models
 type Product struct {
 	ID          int    `json:"id" gorm:"primarykey:autoIncrement"`
 	Image       string `json:"image" gorm:"type: varchar(500)"`
-	ProductName string `json:"productName" gorm:"type: varchar(255), unique"`
+	ProductName string `json:"productName" gorm:"type: varchar(255)"`
 	Purchase    int    `json:"purchase"`
 	Selling     int    `json:"selling"`
 	Qty         int    `json:"qty"`
